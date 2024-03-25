a0e947e76b1541444ae9163c5cc6d40ba2ed20db
aqilaali1 <aqilaali@hotmail.co.uk>
2024-03-25 00:56:16 +0000

    Renamed variable

--- hello_World copy.py
+++ hello_World copy.py
@@ -1,4 +1,4 @@
 # Programme is storing a value in the variable name as a string 
-saying = input("Enter your saying: ")
+statement = input("Enter your statement: ")
 # Programme is inputting the variable into a sentence which will then be printed
-print(saying)
+print(statement)
